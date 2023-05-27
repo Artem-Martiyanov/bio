7c29b6f26f39d1b0f1cd402ab779ab068820869e
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-05-27 12:36:32 +0300

    Test

--- source/app/app.js
+++ source/app/app.js
@@ -1,1 +1,9 @@
-import Works from '../components/works';const App = {  render: function () {    Works.render()  }}export default App
+import Works from '../components/works';
+
+const App = {
+  render: function () {
+    Works.render()
+  }
+}
+
+export default App
