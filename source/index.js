0eb285f30473a532bb9229bc53cb578cf3d4916b
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-05-27 12:16:58 +0300

    lf config

--- source/index.js
+++ source/index.js
@@ -1,1 +1,11 @@
-import './sass/styles.scss'import initPlugins from './components/plugins/instances';import {initClickHandle} from './app/clickHandler';import {initScrollHandle} from './app/scrollHandle';import App from './app/app';initPlugins()initClickHandle()initScrollHandle()App.render()
+import './sass/styles.scss'
+import initPlugins from './components/plugins/instances';
+import {initClickHandle} from './app/clickHandler';
+import {initScrollHandle} from './app/scrollHandle';
+import App from './app/app';
+
+initPlugins()
+initClickHandle()
+initScrollHandle()
+
+App.render()
