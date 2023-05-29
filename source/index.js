67877134be0b485a5b54b90ba76d7f6011defa1f
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-05-29 23:05:41 +0300

    Removes unnecessary

--- source/index.js
+++ source/index.js
@@ -3,7 +3,6 @@ import initPlugins from './components/plugins/instances';
 import {initClickHandle} from './app/clickHandler';
 import {initScrollHandle} from './app/scrollHandle';
 import App from './app/app';
-import Store from './store/store';
 
 
 initPlugins()
@@ -11,11 +10,3 @@ initClickHandle()
 initScrollHandle()
 
 App.render()
-
-
-const store = new Store()
-store.actions.works.load().then(() => {
-   const list = store.actions.works.getList()
-
-
-})
